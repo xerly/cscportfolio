1b6f0dab7f158a120d25ef6c735d900bf3bc1703
Carles Sánchez <citmrulez@gmail.com>
2021-07-06 17:09:33 +0200

    Deploy

---
+++ src/components/About.js
@@ -0,0 +1,45 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <section id="about">
+      <div className="container mx-auto flex px-10 py-20 md:flex-row flex-col items-center">
+        <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
+          <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">
+            Hi, I'm Carles.
+            <br className="hidden lg:inline-block" />I love to build amazing
+            apps.
+          </h1>
+          <p className="mb-8 leading-relaxed">
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eaque
+            officiis vitae illum modi unde reprehenderit, ut in necessitatibus
+            incidunt esse!
+          </p>
+          <div className="flex justify-center">
+            <a
+              href="#contact"
+              className="inline-flex text-white bg-green-500 border-0 py-2 px-6 focus:outline-none hover:bg-green-600 rounded text-lg"
+            >
+              Work With Me
+            </a>
+            <a
+              href="#projects"
+              className="ml-4 inline-flex text-gray-400 bg-gran-800 border-0 py-2 px-6 focus:outline-none hover:bg-gray-700 hover:text-white rounded text-lg"
+            >
+              See My Past Work
+            </a>
+          </div>
+        </div>
+        <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
+          <img
+            src="./coding.svg"
+            alt="hero"
+            className="object-cover object-center rounded"
+          />
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default About;
